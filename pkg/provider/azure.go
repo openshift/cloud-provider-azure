04f8b7e036c115988bf7fd7bc903808a1215793e
Damiano Donati <damiano.donati@gmail.com>
2023-03-10 12:54:46 +0100

    UPSTREAM: <carry>: do not detach masters from lb when unready

--- pkg/provider/azure.go
+++ pkg/provider/azure.go
@@ -1145,7 +1145,7 @@ func (az *Cloud) updateNodeCaches(prevNode, newNode *v1.Node) {
 		case hasExcludeBalancerLabel:
 			az.excludeLoadBalancerNodes.Insert(newNode.ObjectMeta.Name)
 
-		case !isNodeReady(newNode) && nodemanager.GetCloudTaint(newNode.Spec.Taints) == nil:
+		case !isNodeReady(newNode) && !isNodeMaster(newNode) && nodemanager.GetCloudTaint(newNode.Spec.Taints) == nil:
 			// If not in ready state and not a newly created node, add to excludeLoadBalancerNodes cache.
 			// New nodes (tainted with "node.cloudprovider.kubernetes.io/uninitialized") should not be
 			// excluded from load balancers regardless of their state, so as to reduce the number of
@@ -1299,5 +1299,16 @@ func isNodeReady(node *v1.Node) bool {
 			return true
 		}
 	}
+	return false
+}
+
+func isNodeMaster(node *v1.Node) bool {
+	labels := node.GetLabels()
+	for l := range labels {
+		if l == "node-role.kubernetes.io/master" || l == "node-role.kubernetes.io/control-plane" {
+			return true
+		}
+	}
+
 	return false
 }
