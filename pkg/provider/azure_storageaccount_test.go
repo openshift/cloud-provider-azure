55979005613e37066d36d5613160e78eed065e95
Fabio Bertinatto <fbertina@redhat.com>
2023-07-17 17:16:36 -0300

    UPSTREAM: 4203: fix: storage account search default values

--- pkg/provider/azure_storageaccount_test.go
+++ pkg/provider/azure_storageaccount_test.go
@@ -825,7 +825,7 @@ func TestIsAllowBlobPublicAccessEqual(t *testing.T) {
 			accountOptions: &AccountOptions{
 				AllowBlobPublicAccess: pointer.Bool(false),
 			},
-			expectedResult: true,
+			expectedResult: false,
 		},
 		{
 			account: storage.Account{
@@ -845,7 +845,7 @@ func TestIsAllowBlobPublicAccessEqual(t *testing.T) {
 			accountOptions: &AccountOptions{
 				AllowBlobPublicAccess: pointer.Bool(true),
 			},
-			expectedResult: false,
+			expectedResult: true,
 		},
 		{
 			account: storage.Account{
@@ -879,7 +879,7 @@ func TestIsAllowSharedKeyAccessEqual(t *testing.T) {
 				},
 			},
 			accountOptions: &AccountOptions{},
-			expectedResult: false,
+			expectedResult: true,
 		},
 		{
 			account: storage.Account{
@@ -888,7 +888,7 @@ func TestIsAllowSharedKeyAccessEqual(t *testing.T) {
 			accountOptions: &AccountOptions{
 				AllowSharedKeyAccess: pointer.Bool(false),
 			},
-			expectedResult: true,
+			expectedResult: false,
 		},
 		{
 			account: storage.Account{
@@ -908,7 +908,7 @@ func TestIsAllowSharedKeyAccessEqual(t *testing.T) {
 			accountOptions: &AccountOptions{
 				AllowSharedKeyAccess: pointer.Bool(true),
 			},
-			expectedResult: false,
+			expectedResult: true,
 		},
 		{
 			account: storage.Account{
@@ -944,7 +944,7 @@ func TestIsRequireInfrastructureEncryptionEqual(t *testing.T) {
 				},
 			},
 			accountOptions: &AccountOptions{},
-			expectedResult: false,
+			expectedResult: true,
 		},
 		{
 			account: storage.Account{
